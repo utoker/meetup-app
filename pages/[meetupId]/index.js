dace4c47f57d86d2934a3e25ffee1c3c8e9c0219
Umut Toker <utoker@gmail.com>
2021-12-30 15:21:17 -0500

    add meetup detail page

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://worldbulletin.dunyabulteni.net/images/haberler/250x190/2012/08/07/img-3369.jpg"
+      title="title"
+      address="address"
+      description="description"
+    />
+  );
+};
+
+export default MeetupDetails;
