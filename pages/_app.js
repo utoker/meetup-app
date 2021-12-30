44c5ffbc88f89d83c146413ec8d3671770f81933
Umut Toker <utoker@gmail.com>
2021-12-30 13:32:43 -0500

    add layout

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,11 @@ import "../styles/globals.css";
 import Layout from "../components/layout/Layout";
 
 function MyApp({ Component, pageProps }) {
-  return;
-  <Layout>
-    <Component {...pageProps} />
-  </Layout>;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
